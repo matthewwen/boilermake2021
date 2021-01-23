5dd426720ef25896b51220c9871560ac59118b28
Matthew Wen <mattwen2018@gmail.com>
2021-01-23 12:58:41 -0500

    Added Captial One Nessie

--- purdue_brain/discord_config.py
+++ purdue_brain/discord_config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from discord.ext import commands
 import random
 
+from purdue_brain.commands.AddNessie import UserGetApiLink, UserAddApiKey
 from purdue_brain.commands.HelloWorld import UserCommandHelloWorld
 from purdue_brain.commands.NewCommand import UserCommandNewCommand
 from purdue_brain.commands.price import UserCommandPrice
@@ -28,6 +29,7 @@ async def on_ready():
 
 def create_direct_command(content):
     return iterate_commands(content, [
+        ('$help_bank', UserGetApiLink), ('$add_bank_key', UserAddApiKey),
         ('$hi', UserCommand), ('$helloworld', UserCommandHelloWorld),
         ('$natalie', UserCommandNewCommand), ('$price', UserCommandPrice), ('$info', UserCommandInfo)
     ])
