7a33967368278d9c5fe394f2acad8c493bee648b
Matthew Wen <mattwen2018@gmail.com>
2021-01-24 00:19:36 -0600

    Feature discord dm (#9)
    * equity
    
    * added daily change
    
    * Error Handle Accounts
    
    * Clean up code
    
    * Split Discord and DM
    
    Co-authored-by: brianmlatimer <blatimer@purdue.edu>

--- purdue_brain/commands/details.py
+++ purdue_brain/commands/details.py
@@ -5,11 +5,12 @@ import os
 
 from purdue_brain.common.utils import get_attribute
 
-
-def findEquity(market_value, crypto, total_equity, change):
-    message = create_simple_message('Total Equity', '${:,.2f}'.format(float(total_equity)))
-    message = create_simple_message('Stock Value', '${:,.2f}'.format(float(market_value)), embed=message)
-    message = create_simple_message('Crypto Value', '${:,.2f}'.format(float(crypto)), embed=message)
+def findEquity(market_value, crypto, total_equity, change, show_all_equity=True):
+    message = None
+    if show_all_equity:
+        message = create_simple_message('Total Equity', '${:,.2f}'.format(float(total_equity)), embed=message)
+        message = create_simple_message('Stock Value', '${:,.2f}'.format(float(market_value)), embed=message)
+        message = create_simple_message('Crypto Value', '${:,.2f}'.format(float(crypto)), embed=message)
     message = create_simple_message('Daily Change', '{:,.5f}%'.format(float(change)), embed=message)
     return message
 
@@ -18,6 +19,7 @@ class UserCommandDetails(UserCommand):
 
     def __init__(self, author, content, response: UserResponse):
         super().__init__(author, content, response)
+        self.show_all = True
 
     async def run(self):
         r.login(os.getenv('ROBINHOOD_USERNAME'), os.getenv('ROBINHOOD_PASSWORD'))
@@ -28,7 +30,7 @@ class UserCommandDetails(UserCommand):
         curr = float(get_attribute(profile, ['portfolio_equity', 'amount'], default=0))
         prev = float(get_attribute(profile, ['portfolio_previous_close', 'amount'], default=0))
         change = (curr - prev) / prev * 100
-        message = findEquity(market_value, crypto_value, equity, change)
+        message = findEquity(market_value, crypto_value, equity, change, self.show_all)
         self.response.add_response(message)
 
         if len(self.response.response) == 0:
