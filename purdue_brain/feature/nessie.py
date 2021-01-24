677acf1ec62c6f467bc25074f12536606b0f96cc
Matthew Wen <mattwen2018@gmail.com>
2021-01-23 21:12:14 -0500

    Randomly handing a purchase every 10 minutes

--- purdue_brain/feature/nessie.py
+++ purdue_brain/feature/nessie.py
@@ -1,7 +1,11 @@
 import json
 import os
+from datetime import datetime
+
 import requests
 from dotenv import load_dotenv
+
+from purdue_brain.wrappers.discord_wrapper import DiscordWrapper
 
 load_dotenv()
 endpoint_url = "http://api.nessieisreal.com"
@@ -17,10 +21,53 @@ def process_request(url, payload):
     return response is not None and response.status_code == 201, response
 
 
+def process_get(url):
+    response = requests.get(
+        url=url
+    )
+    return response is not None and response.status_code == 200, response
+
+
 def create_content(response):
-    if response.status_code == 201:
-        my_json = response.content.decode('utf8').replace("'", '"')
+    if response.status_code in [200, 201, 202]:
+        my_json = response.content.decode('utf8')
         return json.loads(my_json)
+    return None
+
+
+def get_merchants():
+    url = endpoint_url + f"/merchants?lat={40.42353}&lng={-86.921738}&rad={50}&key={api_key}"
+    is_valid, response = process_get(url)
+    content = create_content(response)
+    return content if is_valid else None
+
+
+def get_robinhood_merchant():
+    config = DiscordWrapper.fire.get_discord_config()
+    if 'robinhood_merchant_id' in config:
+        return config['robinhood_merchant_id']
+
+    url = endpoint_url + f"/merchants?key={api_key}"
+    payload = {
+        "name": "Robinhood",
+        "address": {
+            "street_number": "84",
+            "street_name": "Willow Road",
+            "city": "Menlo Park",
+            "state": "CA",
+            "zip": "94025"
+        },
+        "geocode": {
+            "lat": 37.452961,
+            "lng": -122.181725
+        }
+    }
+    is_valid, response = process_request(url, payload)
+    content = create_content(response)
+    if is_valid:
+        robinhood_id = content['objectCreated']
+        DiscordWrapper.fire.set_discord_config({'robhinhood_merchant_id': robinhood_id})
+        return robinhood_id
     return None
 
 
@@ -29,6 +76,18 @@ class Nessie:
     def __init__(self, customer_id, account_id):
         self.customer_id = customer_id
         self.account_id = account_id
+
+    def get_customer_data(self):
+        url = endpoint_url + f"/accounts/{self.account_id}?key={api_key}"
+        is_valid, response = process_get(url)
+        content = create_content(response)
+        return content if is_valid else None
+
+    def get_purchases(self):
+        url = endpoint_url + f"/accounts/{self.account_id}/purchases?key={api_key}"
+        is_valid, response = process_get(url)
+        content = create_content(response)
+        return content if is_valid else None
 
     def create_customer(self):
         url = endpoint_url + f"/customers?key={api_key}"
@@ -47,6 +106,40 @@ class Nessie:
         content = create_content(response)
         self.customer_id = content['objectCreated']['_id'] if is_valid else None
         return self.customer_id is not None
+
+    def perform_purchase(self, amount, merchant_id=None, description='no description'):
+        if merchant_id is None:
+            merchant_id = get_robinhood_merchant()
+            if description == 'no description':
+                description = 'Bought Stock on Robinhood'
+
+        url = endpoint_url + f"/accounts/{self.account_id}/purchases?key={api_key}"
+        now = datetime.now()
+        payload = {
+            "merchant_id": merchant_id,
+            "medium": "balance",
+            "purchase_date": f'{now.strftime("%Y-%m-%d")}',
+            "amount": amount,
+            "status": "completed",
+            "description": description
+        }
+        is_valid, response = process_request(url, payload)
+        content = create_content(response)
+        return content if is_valid else None
+
+    def add_money_to_account(self, amount, description='no description'):
+        url = endpoint_url + f'/accounts/{self.account_id}/deposits?key={api_key}'
+        now = datetime.now()
+        payload = {
+            "medium": "balance",
+            "transaction_date": f'{now.strftime("%Y-%m-%d")}',
+            "status": "completed",
+            'amount': amount,
+            "description": description
+        }
+        is_valid, response = process_request(url, payload)
+        content = create_content(response)
+        return content if is_valid else None
 
     def create_account(self):
         if self.customer_id is not None:
