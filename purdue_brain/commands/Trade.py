43ee11b12a28e0a4cf653b79abb07515afe02b34
clarye <54417595+clarye@users.noreply.github.com>
2021-01-24 02:21:47 -0500

    Update Trade.py

--- purdue_brain/commands/Trade.py
+++ purdue_brain/commands/Trade.py
@@ -5,9 +5,9 @@ from purdue_brain.common import UserResponse, create_simple_message
 import robin_stocks as r
 import os
 
-def order_information(name, symbol, quantity, buy_sell=None, limit_price=None, stop_price=None, timeout_code=None):
+def order_information(symbol, quantity, buy_sell=None, limit_price=None, stop_price=None, timeout_code=None):
 
-    message = create_simple_message('Company', name)
+    message = create_simple_message('Company', symbol)
     message = create_simple_message('Symbol', symbol, embed=message)
     message = create_simple_message('Quantity', quantity, embed=message)
     message = create_simple_message('Buy or Sell', buy_sell, embed=message)
@@ -29,82 +29,82 @@ class UserCommandTrade(UserCommand):
         buy_sell = 'N/A'
         if '$order ' in self.content:
             stock = self.content.replace('$order ', '').upper().split()
-            name, symbol, quantity, buy_sell, limit_price, stop_price, timeout_code = stock
+            symbol, quantity, buy_sell, limit_price, stop_price, timeout_code = stock
             confirmation = r.order(str(symbol), int(quantity), (str(buy_sell)).lower(), float(limit_price),
                                    float(stop_price), (str(timeout_code)).lower())
 
         elif '$order_buy_market ' in self.content:
             stock = self.content.replace('$order_buy_market ', '').upper().split()
-            name, symbol, quantity, timeout_code = stock
+            symbol, quantity, timeout_code = stock
             confirmation = r.order_buy_market(str(symbol), int(quantity), str(timeout_code).lower())
 
         elif '$order_sell_market ' in self.content:
             stock = self.content.replace('$order_sell_market ', '').upper().split()
-            name, symbol, quantity, buy_sell, limit_price, stop_price, timeout_code = stock
+            symbol, quantity, buy_sell, limit_price, stop_price, timeout_code = stock
             confirmation = r.order_sell_market(str(symbol), int(quantity), str(timeout_code).lower())
 
         elif '$order_buy_limit ' in self.content:
             stock = self.content.replace('$order_buy_limit ', '').upper().split()
-            name, symbol, quantity, limit_price, timeout_code = stock
+            symbol, quantity, limit_price, timeout_code = stock
             confirmation = r.order_buy_limit(str(symbol), int(quantity), float(limit_price), str(timeout_code).lower())
 
         elif '$order_sell_limit ' in self.content:
             stock = self.content.replace('$order_sell_limit ', '').upper().split()
-            name, symbol, quantity, limit_price, timeout_code = stock
+            symbol, quantity, limit_price, timeout_code = stock
             confirmation = r.order_sell_limit(str(symbol), int(quantity), float(limit_price), str(timeout_code).lower())
 
         elif '$order_buy_stop_loss ' in self.content:
             stock = self.content.replace('$order_buy_stop_loss ', '').upper().split()
-            name, symbol, quantity, stop_price, timeout_code = stock
+            symbol, quantity, stop_price, timeout_code = stock
             confirmation = r.order_buy_stop_loss(str(symbol), int(quantity), float(stop_price),
                                                  str(timeout_code).lower())
 
         elif '$order_sell_stop_loss ' in self.content:
             stock = self.content.replace('$order_sell_stop_loss ', '').upper().split()
-            name, symbol, quantity, stop_price, timeout_code = stock
+            symbol, quantity, stop_price, timeout_code = stock
             confirmation = r.order_sell_stop_loss(str(symbol), int(quantity), float(stop_price),
                                                   str(timeout_code).lower())
 
         elif '$order_buy_trailing_stop ' in self.content:
             stock = self.content.replace('$order_buy_trailing_stop ', '').upper().split()
-            name, symbol, quantity, trailAmount, trailType, timeout_code = stock
+            symbol, quantity, trailAmount, trailType, timeout_code = stock
             confirmation = r.order_buy_trailing_stop(str(symbol), int(quantity), float(trailAmount),
                                                      (str(trailType)).lower(), str(timeout_code).lower())
 
         elif '$order_sell_trailing_stop ' in self.content:
             stock = self.content.replace('$order_sell_trailing_stop ', '').upper().split()
-            name, symbol, quantity, trailAmount, trailType, timeout_code = stock
+            symbol, quantity, trailAmount, trailType, timeout_code = stock
             confirmation = r.order_sell_trailing_stop(str(symbol), int(quantity), float(trailAmount), str(trailType),
                                                       str(timeout_code).lower())
 
         elif '$order_sell_stop_limit ' in self.content:
             stock = self.content.replace('$order_sell_stop_limit ', '').upper().split()
-            name, symbol, quantity, limit_price, stop_price, timeout_code = stock
+            symbol, quantity, limit_price, stop_price, timeout_code = stock
             confirmation = r.order_sell_stop_limit(str(symbol), int(quantity), float(limit_price), float(stop_price),
                                                    (str(timeout_code)).lower())
 
         elif '$order_buy_crypto_limit_by_price ' in self.content:
             stock = self.content.replace('$order_buy_crypto_limit_by_price ', '').upper().split()
-            name, symbol, amountInDollars, limit_price, timeout_code = stock
+            symbol, amountInDollars, limit_price, timeout_code = stock
             print(str(symbol), float(amountInDollars), float(limit_price), (str(timeout_code)).lower())
             confirmation = r.order_buy_crypto_limit_by_price(str(symbol), float(amountInDollars), float(limit_price), (str(timeout_code)).lower())
 
         elif '$order_buy_crypto_by_quantity ' in self.content:
             stock = self.content.replace('$order_buy_crypto_by_quantity ', '').upper().split()
-            name, symbol, quantity, timeout_code = stock
+            symbol, quantity, timeout_code = stock
             confirmation = r.order_buy_crypto_by_quantity(str(symbol), int(quantity), (str(timeout_code)).lower())
 
         elif '$order_sell_crypto_limit_by_price ' in self.content:
             stock = self.content.replace('$order_sell_crypto_limit_by_price ', '').upper().split()
-            name, symbol, amountInDollars, limit_price, timeout_code = stock
+            symbol, amountInDollars, limit_price, timeout_code = stock
             confirmation = r.order_sell_crypto_limit_by_price(str(symbol), float(amountInDollars), float(limit_price), (str(timeout_code)).lower())
 
         elif 'order_sell_crypto_limit ' in self.content:
             stock = self.content.replace('$order_buy_crypto_by_quantity ', '').upper().split()
-            name, symbol, quantity, limit_price, timeout_code = stock
+            symbol, quantity, limit_price, timeout_code = stock
             confirmation = r.order_sell_crypto_limit(str(symbol), int(quantity), float(limit_price), (str(timeout_code)).lower())
         print(confirmation)
-        message = order_information(name, symbol, quantity, buy_sell, limit_price, stop_price, timeout_code)
+        message = order_information(symbol, quantity, buy_sell, limit_price, stop_price, timeout_code)
 
         if 'id' not in confirmation:
             message = create_simple_message('❌ Order Failed - Details', [j for i,j in confirmation.items()])
